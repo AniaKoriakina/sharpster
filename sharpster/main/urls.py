7ada930d50eefe1937287cd6dfa8bd506078b2de
Ania <coryakina.anna@gmail.com>
2024-01-17 15:14:10 +0500

    add base frontend

--- sharpster/main/urls.py
+++ sharpster/main/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name="home"),
+    path('trends', views.trends, name="trends"),
+    path('geography', views.geography, name="geography"),
+    path('skills', views.skills, name="skills"),
+    path('vacancies', views.vacancies, name="vacancies"),
 ]
--- sharpster/main/views.py
+++ sharpster/main/views.py
@@ -5,4 +5,20 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Добро пожаловать</h1>")
+    return render(request, 'main/index.html')
+
+
+def trends(request):
+    return render(request, 'main/index.html')
+
+
+def geography(request):
+    return render(request, 'main/index.html')
+
+
+def skills(request):
+    return render(request, 'main/index.html')
+
+
+def vacancies(request):
+    return render(request, 'main/index.html')
